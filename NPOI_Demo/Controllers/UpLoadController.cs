7dc5b08385be0fb2601025a08de91faed64a4240
bk lin at home <kl13245768@gmail.com>
2022-08-08 01:16:27 +0800

    add NPOI read

--- NPOI_Demo/Controllers/UpLoadController.cs
+++ NPOI_Demo/Controllers/UpLoadController.cs
@@ -5,34 +5,58 @@ using System.Linq;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace NPOI_Demo.Controllers
 {
 	public class UpLoadController : Controller
 	{
-		// GET: UpLoad
 		[HttpPost]
 		public ActionResult Uploads(HttpPostedFileBase file)
 		{
-			//判斷是否有上傳檔案
+			UploadService(file);
+
+			ShowExcelValue();
+			return View("ExcelImport");
+		}
+
+		private void ShowExcelValue()
+		{
+			//if (!ViewBag.path) return;
+
+			IWorkbook workBook = new XSSFWorkbook(ViewBag.path);
+			ISheet sheet = workBook.GetSheetAt(0);
+
+			var formula = new XSSFFormulaEvaluator(workBook);
+			var first_Cell = sheet.GetRow(0).GetCell(0);
+			var second_Cell = sheet.GetRow(0).GetCell(1);
+
+
+			ViewBag.First_Cell_Value = first_Cell.ToString();
+			ViewBag.Second_Cell_Formula = second_Cell.ToString();
+			formula.EvaluateFormulaCell(second_Cell);
+			ViewBag.Second_Cell_Nermeric = second_Cell.NumericCellValue.ToString();
+
+		}
+
+		public void UploadService(HttpPostedFileBase file)
+		{
 			if (Request.Files["file"].ContentLength > 0)
 			{
 				string extension = System.IO.Path.GetExtension(file.FileName);
 				string fileSavedPath = WebConfigurationManager.AppSettings["UploadPath"];
 
-				//判斷是否為excel檔
 				if (extension == ".xls" || extension == ".xlsx")
 				{
-					//更改檔名為當天日期時間
 					string newFileName = string.Concat(
 					DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"),
 					Path.GetExtension(file.FileName).ToLower());
 					string fullFilePath = Path.Combine(Server.MapPath(fileSavedPath), newFileName);
 
-					// 存放檔案到伺服器上
 					Request.Files["file"].SaveAs(fullFilePath);
 
-					//將資料傳送到前端顯示
 					ViewBag.path = fullFilePath;
 					Response.Write("<script language=javascript>alert(' 檔案上傳成功 ');</" +
 					"script>");
@@ -50,8 +74,6 @@ namespace NPOI_Demo.Controllers
 			{
 				Response.Write("<script language=javascript>alert('請選擇檔案');</" + "script>");
 			}
-
-			return View("ExcelImport");
 		}
 	}
 }
